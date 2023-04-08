031feb4ea2b62c73875f2f97097ea3b06567f830
Jimmy Li <jmyli562@gmail.com>
2023-04-08 14:17:55 -0700

    finishes lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function(e){
+    let selectText = document.getElementById("text")
+    selectText.textContent = "This is really cool!";
+})
